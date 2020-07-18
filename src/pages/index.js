0b04b344056f64ce654b99ecd4950d218f21b1ff
Marcus Battle <themarcusbattle@gmail.com>
2020-07-18 19:50:06 -0400

    Added first manifesto

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,13 +8,22 @@ import SEO from "../components/seo"
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to Ntro</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
+    <p>Ntro connects new developers with real-world experience, to help them land their first job.</p>
+    <p>Securing your first coding job shouldn’t be that difficult. You’ve already spent hours, possibly years, consuming every online academy and tutorial you could get your browser on. But when junior developer positions require three years of experience, new developers, such as yourself ,find the barrier to entry nearly impossible.</p>
+    <h2>Programming experience for new developers</h2>
+    <p>“I’ve finished my bootcamp. Why can’t the company just train me to do the job?” -Every new developer</p>
+    <p>While the current system has its flaws, it’s requirements actually make sense. While you need experience, the truth is, companies don’t have the time or resources to train you. Managing an apprenticeship requires companies to spend time away from their primary business.</p>
+    <p>That’s why Ntro is your chance to finally add some real-word experience to your knowledge and land that first tech role!</p>
+    <h2>Own or manage a Startup?</h2>
+    <p>Your startup provides a publicly accessible repo to Ntro. Ntro then pairs you with a qualified new developer to fix (or even create new) Github issues. Those pull requests are then code reviewed by a senior engineer at Ntro who provides you with a ready-to-merge pull request. That’s it!</p>
+    <p>Imagine your bootstrapped startup receiving notifications for twenty pull requests for your latest web property!</p>
+    <p>Your company sets the price for pull requests and Ntro developers are paid 80% of the proceeds.</p>
+    <p>Everyone wins! You get your time to focus on actually building your business, and Ntro developers build their resumes.</p>
+    <p>Who knows? Maybe they’ll even be your next junior engineer?</p>
+    <p>As partner of Ntro, your company will receive a digital badge of honor on our website to help promote your business as an official “Ntro Partner”.</p>
+    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem`, display: `none` }}>
       <Image />
     </div>
-    <Link to="/page-2/">Go to page 2</Link> <br />
     <Link to="/using-typescript/">Go to "Using TypeScript"</Link>
   </Layout>
 )
