bd864ffacf668a9c76f789598cbf67b01fbdb126
Marcus Battle <themarcusbattle@gmail.com>
2020-07-20 03:19:41 -0400

    Completed first draft of home page

--- src/pages/index.js
+++ src/pages/index.js
@@ -16,27 +16,23 @@ const HomePage = () => (
         <div className="sm:w-3/4 text-gray-700">
           <h1 className="text-4xl sm:text-6xl leading-none mb-8 sm:mb-10">We connect new developers with real-world experience</h1>
           <p className="text-xl mr-20 sm:mr-0 sm:text-2xl font-light mb-20">To help them land their first job!</p>
-          <a href="#" className="bg-gray-700 text-white px-5 py-3 rounded cursor-pointer sm:text-lg sm:px-8 sm:py-4 mr-4">Apply Now</a>
-          <Link to="/about" className="bg-gray-200 px-5 py-3 rounded cursor-pointer sm:text-lg sm:px-8 sm:py-4">Learn More</Link>
+          <a href="https://forms.gle/QJ1zHKnmmRskDq3P9" className="bg-gray-700 text-white px-5 py-3 rounded cursor-pointer sm:text-lg sm:px-8 sm:py-4 mr-4">Apply Now</a>
+          <Link to="#about" className="bg-gray-200 px-5 py-3 rounded cursor-pointer sm:text-lg sm:px-8 sm:py-4">Learn More</Link>
         </div>
       </div>
     </div>
 
-    <div className="bg-gray-700 px-5 sm:px-20 py-20 text-white">
-      <h1 className="text-4xl">Own a startup?</h1>
-    </div>
-    <div className="border-t border-gray-200 px-5 py-20 sm:px-20 text-base font-light">
-      <div className="sm:w-1/2 mx-auto leading-relaxed sm:text-center">
-        <h2 className="text-4xl leading-none text-gray-700 mb-8">Securing your first coding job shouldn’t be that difficult.</h2>
-        <p>You’ve already spent weeks, possibly years, consuming every online academy and tutorial you could get your browser on. But when junior developer positions require three years of experience, new developers, such as yourself, find the barrier to entry nearly impossible.</p>
-        <p>“I’ve finished my bootcamp. Why can’t the company just train me to do the job?” -Every new developer</p>
+    <div id="about" className="bg-gray-700 px-5 sm:px-20 py-40 text-white leading-loose">
+      <div className="sm:w-3/4">
+        <h1 className="text-4xl mb-8 leading-none">An apprenticeship for new developers</h1>
+        <p className="mb-6">Securing your first coding job shouldn’t be that difficult. You’ve already spent weeks, possibly years, consuming every online academy and tutorial you could get your browser on. But when junior developer positions require three years of experience, new developers such as yourself, find the barrier to entry nearly impossible.</p>
+        <p className="mb-6">While the current system has its flaws, it’s requirements actually make sense. While you need experience, the truth is, companies don’t have the time or resources to train you. Managing an apprenticeship requires companies to spend time away from their primary business.</p>
+        <p>That’s why Ntro is your chance to finally add some real-word experience to your knowledge and land that first tech role!</p>
       </div>
     </div>
-    <div className="border-t">
-      <h2>Programming experience for new developers</h2>
-      
-      <p>While the current system has its flaws, it’s requirements actually make sense. While you need experience, the truth is, companies don’t have the time or resources to train you. Managing an apprenticeship requires companies to spend time away from their primary business.</p>
-      <p>That’s why Ntro is your chance to finally add some real-word experience to your knowledge and land that first tech role!</p>
+
+    <div className="p-5 sm:px-20 text-xs">
+      <p>&copy; 2020 Ntro. Questions? Email ntro at hey dot com</p>
     </div>
   </div>
 )
